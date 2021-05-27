f7c32553234c9101456e8d058a4ea4f81835ef47
yazeed alshami <alshamiyazeed@gmail.com>
2021-05-27 17:18:48 +0300

    fixed

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
-  let array =[testArray[0]+testArray[1]+testArray[2],'2,3,4 was passed in as an array of numbers, and 9 is their sum.']
+  let array =[sumAndMultiply(testArray[0],testArray[1],testArray[2])[0],'2,3,4 was passed in as an array of numbers, and 9 is their sum.']
   //eslint-disable-line
   return array;
 }
