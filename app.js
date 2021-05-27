42648072fbbb912beb64b268b170d9046f3ae864
marahmusleh <marahaltarefe18@gmail.com>
2021-05-27 17:54:45 +0300

    solve problem4

--- app.js
+++ app.js
@@ -53,7 +53,6 @@ function sumAndMultiply(a, b, c) {
   let m = multiply(f,c)[0];
   let StringSum = `${a} and ${b} and ${c} sum to ${v}.`;
   let StringMultiply = `The product of ${a} and ${b} and ${c} is ${m}.`;
-  console.log(StringSum,StringMultiply);
   return[v,m,StringSum,StringMultiply];
 }
 testSumAndMultiply(4,7,5);
@@ -75,12 +74,13 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
-  //eslint-disable-line
+  let s = sum(sumArr[0],sumArr[1])[0];
+  let v = sum(s,sumArr[2])[0];
+  let stringarr = testArray + ` was passed in as an array of numbers, and ${v} is their sum.`;
+  console.log(stringarr);
+  return[v,stringarr];
 }
-
-// Here is the test for sumArray(); uncomment it to run it
-
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
