e6d5e0f6495cd4485e2163955ff7a1aeeb7ee222
zaidsh98 <sha7rouri29@gmail.com>
2021-05-27 16:51:04 +0300

    solving 5

--- app.js
+++ app.js
@@ -120,15 +120,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 
-/*function multiplyArray(multArr) {
-   let multiZ = multiply((multArr[0],multArr[1])[0],multArr[2])[0];
-   let stringZ = `The number ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${multiZ}.`;
-   return[multiZ,stringZ];
+function multiplyArray(multArr) {
+   let multiZ = multiply(multiply(multArr[0],multArr[1])[0],multArr[2])[0];
+   let stringZ = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${multiZ}.`;
+   return [multiZ,stringZ];
 }
-*/
+
 
 // Here is the test for multiplyArray(); uncomment it to run it
- //testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
