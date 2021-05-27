f77009e4c5a69bc46b1cc09f89a7ba6520cd0d4b
omaimah-sulaiman <omaymahshakhatrah@gmail.com>
2021-05-27 16:05:38 +0300

    solve problem4

--- app.js
+++ app.js
@@ -53,7 +53,6 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-
   let sum1 =sum(a,b)[0];
   let sum2 =sum(sum1,c)[0];
   let multi=multiply(a,b)[0];
@@ -62,8 +61,6 @@ function sumAndMultiply(a, b, c) {
   let stringMulti =`The product of ${a} and ${b} and ${c} is ${multi1}.`
   return[stringSum,stringMulti]
 }
-
-
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4,7,5);
@@ -82,14 +79,24 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
-
+let sum1 =0
+let stringArr=''
 function sumArray(sumArr) {
-  //eslint-disable-line
+  for (let i=0;i<testArray.length;i++){
+     sum1 =sum(testArray[i],sum1)[0]
+     if (i<2){
+       stringArr=stringArr+testArray[i]+','
+     }else{
+      stringArr=stringArr+testArray[i]
+     }
+  }
+   let st=`${stringArr} was passed in as an array of numbers, and ${sum1} is thier sum.`
+   return[sum1,st]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
