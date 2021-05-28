ec510c8fdd47b6e242a8bf80eafc2baff7fcc481
asyamoh <asia_nazif@yahoo.com>
2021-05-28 23:33:43 +0300

    add

--- app.js
+++ app.js
@@ -8,29 +8,21 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-<<<<<<< HEAD
+
 let c = a + b
 return [c,"The sum of " + a + " and " + b + " is " + c + "."]
-=======
-
-
-//sum();
 
 function sum(a,b){
   let x = a + b;
   return [x,"The sum of " + a + " and " + b + " is " + x + "."];
   
->>>>>>> 1f5c2eb7ce626df1380127ca0757cfa2500fd23f
 }
 sum(4,7);
 
 
 // Here is the test for sum(); uncomment it to run it
-<<<<<<< HEAD
+
  testSum(4, 7);
-=======
- //testSum(4, 7);
->>>>>>> 1f5c2eb7ce626df1380127ca0757cfa2500fd23f
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -82,17 +74,6 @@ function sumAndMultiply(a, b,c) {
   // Here is the test for sumAndMultiply(); uncomment it to run it
   testSumAndMultiply(4,7,5);
      
-  
- 
-  //function multiply (a,b){
-  //let f = a * b;
-  //return[f];
-//}
-
-//}
-
-
-
 
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -112,7 +93,6 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 let testArray = [2, 3, 4];
-//eslint-disable-line
 
 function sumArray(sumArr) {
   function sumArray([a,b,c]){
@@ -122,11 +102,10 @@ function sumArray(sumArr) {
 }
 sumArray([]);
 }
-sumArray(testArray);
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
